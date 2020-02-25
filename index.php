9bc9d205e22a3c7a31ab0cef787183b87e403c44
Tetyana <tsergata@gmail.com>
2020-02-25 16:59:46 +0100

    Struttura base + collegamenti

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+include __DIR__ . '/server.php';
+
+
+
+
+
+ ?>
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <!-- AJAX -->
+    <script src="https://ajax.googleapis.com/ajax/libs/d3js/5.15.0/d3.min.js"></script>
+    <!-- CSS SAS -->
+    <link rel="stylesheet" href="dist/app.css">
+    <title>Ajax-Dischi</title>
+  </head>
+  <body>
+    <script src="dist/app.js" charset="utf-8"></script>
+  </body>
+</html>
